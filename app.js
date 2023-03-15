6cc9e5c4474376c8d7c0c35d3f84f683b745ca44
Jajhoey <john.hoey15@my.stjohns.edu>
2023-03-14 20:13:14 -0400

    Thrusters now working correctly. Also added friction to slow ship

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 const startButton = document.getElementById('start');
 const turn_speed = 360; //Degrees of rotation per second
-const friction = 0.7; //Coefficient of friction in space
+const friction = 0.7; //Coefficient of friction
+const ship_acceleration = 10; //increase velocity by 5 pixels per second
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const ship_speed = canvas.width / 300;
 
 
 
@@ -15,7 +15,7 @@ const ship = {
   //Ship position
   x: canvas.width / 2,
   y: canvas.height / 2,
-  radius: 20,
+  radius: 40,
   //Angle is defaulted to 90 (facing north). Angle is converted to radians
   //for the trig functions to work.
   angle: 90/180 * Math.PI,
@@ -56,22 +56,8 @@ function drawShip() {
   context.closePath(); //drawing back to the nose
   context.stroke();
 
-  //rotate the ship
-  ship.angle += ship.rotation / 180 * Math.PI;
-  ship.rotation = 0;
-  //move ship
-  if(ship.thrusting){
-    ship.x += ship.velocity.x;
-    ship.y -= ship.velocity.y;
-    //force of friction
-    ship.velocity.x -= friction * Math.cos(ship.angle);
-    ship.velocity.y -= friction * Math.sin(ship.angle);
-  }
+  //drawing thrust
 
-  if (!ship.thrusting){
-    ship.velocity.x = 0;
-    ship.velocity.y = 0;
-  }
 }
 
 function clearCanvas() {
@@ -79,39 +65,90 @@ function clearCanvas() {
   context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+//this update function is the main game loop
 function update() {
+    //rotate the ship
+    ship.angle += ship.rotation / 180 * Math.PI;
+    ship.rotation = 0;
+
+    //move ship
+    ship.x += ship.velocity.x;
+    ship.y -= ship.velocity.y;
+
+    if (ship.thrusting){
+      ship.velocity.x += ship_acceleration * Math.cos(ship.angle) / 60;
+      ship.velocity.y += ship_acceleration * Math.sin(ship.angle) / 60;
+
+    }
+
+   ship.velocity.x -= friction * ship.velocity.x / 60;
+   ship.velocity.y -= friction * ship.velocity.y / 60;
+
+   //check canvas boundaries for collision and warp ship to opposite side
+   if (ship.x - ship.radius < 0){
+     ship.x = canvas.width - ship.radius;
+   }else if (ship.x + ship.radius > canvas.width) {
+     ship.x = 0 + ship.radius;
+   }
+
+   if (ship.y - ship.radius < 0) {
+     ship.y = canvas.height - ship.radius;
+   }else if (ship.y + ship.radius > canvas.height) {
+     ship.y = 0 + ship.radius;
+   }
+
+
   clearCanvas();
   drawShip();
 }
 
-function gameControls(){
-  document.addEventListener('keydown', () => {
-    switch (event.key) {
-      case "ArrowUp":
-        ship.thrusting = true;
-        ship.velocity.x += ship_speed * Math.cos(ship.angle);
-        ship.velocity.y += ship_speed * Math.sin(ship.angle);
-        break;
-      case "ArrowLeft":
-        ship.rotation += turn_speed/60;
-        break;
-      case "ArrowRight":
-        ship.rotation -= turn_speed/60;
-        break;
-      case " ":
-      console.log("Shoot");
-        break;
-      case "ArrowDown":
-        ship.thrusting = false;
-      default:
-        return;
-    }
-  })
+//Event handlers for game controls
+document.addEventListener('keydown', () => {
+  switch (event.key) {
+    case "ArrowUp":
+      ship.thrusting = true;
+      break;
+    case "ArrowLeft":
+      ship.rotation += turn_speed/60;
+      break;
+    case "ArrowRight":
+      ship.rotation -= turn_speed/60;
+      break;
+    case " ":
+    console.log("Shoot");
+      break;
+    default:
+      return;
+  }
+});
+
+document.addEventListener('keyup', () => {
+  switch (event.key) {
+    case 'ArrowUp':
+      ship.thrusting = false;
+      break;
+    case 'ArrowLeft':
+      ship.rotation = 0;
+      break;
+    case 'ArrowRight':
+      ship.rotation = 0;
+      break;
+    default:
+
+  }
+});
+
+//debugging
+function test(){
+  console.log(ship.x);
+  console.log(ship.y);
+  console.log(ship.thrusting);
+  console.log(ship.rotation);
 }
 
 function startGame(){
-  gameControls();
-  setInterval(update, 1000 / 60); //Fps is 160th
+  setInterval(test, 1000);
+  setInterval(update, 1000 / 60); //Fps is 1/60th
 }
 
 //displayInstructions();
