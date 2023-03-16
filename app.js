79e05eb600eaaec6a59cfe5cf926cf4d32bfb5a6
Jajhoey <john.hoey15@my.stjohns.edu>
2023-03-16 17:36:02 -0400

    Asteroids are now drawn and move/screen warp

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 const startButton = document.getElementById('start');
+
 const turn_speed = 360; //Degrees of rotation per second
-const friction = 0.6; //Coefficient of friction
-const ship_acceleration = 10; //increase velocity by 5 pixels per second
+const friction = 0.7; //Coefficient of friction
+const ship_acceleration = 20; //increase velocity by 20 pixels per second
 const numAsteroids = 3;
+let asteroids = [];
+
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -84,44 +87,62 @@ function drawShip() {
   }
 }
 
-function newAsteroid(){
-  const asteroid = {
-    x: Math.random(),
-    y: Math.random(),
-    radius: 40,
-    angle: 90/180 * Math.PI,
-    velocity: {
-      x: 0,
-      y: 0
-    },
-    rotation: 0,
-    vertices: 10 + Math.random() * 10
-  }
-  return asteroid;
-}
-
-function createAsteroids(){
-  let asteroids = [];
-
-  //pushing Asteroids
-  for (var i = 0; i < numAsteroids; i++){
-    ast = newAsteroid();
-    asteroids.push(ast);
-    context.fillStyle = 'white';
+function drawAsteroids(){
+  //drawing asteroids
+  for (var i = 0; i < asteroids.length; i++){
+    ast = asteroids[i];
+    context.strokeStyle = 'white';
+    context.lineWidth = ship.radius / 15;
     context.beginPath();
-    //drawing asteroids
-    for(var j = 0; j < ast.vertices; j++){
-      context.moveTo(ast.x - ast.radius * Math.cos(ast.angle), ast.y + ast.radius * Math.sin(ast.angle));
+    context.moveTo(
+      ast.x - ast.radius * ast.vertJaggedness[0] * Math.cos(ast.angle),
+      ast.y + ast.radius * ast.vertJaggedness[0] * Math.sin(ast.angle));
+    for(var j = 1; j <= ast.vertices; j++){
       context.lineTo(
-        ast.x - ast.radius * Math.cos(ast.angle) / (ast.vertices - j),
-        ast.y + ast.radius * Math.sin(ast.angle) / (ast.vertices - j)
+        ast.x - ast.radius * ast.vertJaggedness[j] * Math.cos(ast.angle + j * Math.PI * 2 / ast.vertices),
+        ast.y + ast.radius * ast.vertJaggedness[j] * Math.sin(ast.angle + j * Math.PI * 2 / ast.vertices)
       );
     }
     context.closePath();
     context.stroke();
   }
-
 }
+
+function newAsteroid(){
+  const asteroid = {
+    x: 0,
+    y: 0,
+    radius: ship.radius*2,
+    angle: 90/180 * Math.PI,
+    velocity: {
+      x: Math.random() * 5,
+      y: Math.random() * 5
+    },
+    rotation: (Math.random() * 10 - 5) / 180 * Math.PI,
+    vertices: 5 + Math.random() * 10,
+    vertJaggedness: []
+  }
+  for (var i = 0; i < asteroid.vertices; i++) {
+    asteroid.vertJaggedness.push(Math.random()+1);
+  }
+  do {//reset the coords until the distance is larger than 5x asteroid radius
+    asteroid.x = Math.random() * canvas.width;
+    asteroid.y = Math.random() * canvas.height;
+  } while (distanceBetweenPoints(ship.x, ship.y, asteroid.x, asteroid.y) < asteroid.radius * 5 );
+  return asteroid;
+}
+
+function createAsteroids(){
+  //pushing Asteroids
+  for (var i = 0; i < numAsteroids; i++){
+    ast = newAsteroid();
+    asteroids.push(ast);
+  }
+}
+
+function distanceBetweenPoints(x1, y1, x2, y2){
+  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}// d=√((x2 – x1)² + (y2 – y1)²)
 
 function clearCanvas() {
   context.fillStyle = 'black';
@@ -130,13 +151,19 @@ function clearCanvas() {
 
 //this update function is the main game loop
 function update() {
-    //rotate the ship
+    //rotate the ship and asteroids
     ship.angle += ship.rotation / 180 * Math.PI;
     ship.rotation = 0;
-
-    //move ship
+    for (var i = 0; i < asteroids.length; i++) {
+      asteroids[i].angle += asteroids[i].rotation;
+    }
+    //move ship and asteroids
     ship.x += ship.velocity.x;
     ship.y -= ship.velocity.y;
+    for (var i = 0; i < asteroids.length; i++) {
+      asteroids[i].x += asteroids[i].velocity.x;
+      asteroids[i].y -= asteroids[i].velocity.y;
+    }
 
     if (ship.thrusting){
       ship.velocity.x += ship_acceleration * Math.cos(ship.angle) / 60;
@@ -147,24 +174,46 @@ function update() {
    ship.velocity.x -= friction * ship.velocity.x / 60;
    ship.velocity.y -= friction * ship.velocity.y / 60;
 
-   //check canvas boundaries for collision and warp ship to opposite side
-   if (ship.x - ship.radius < 0){
-     ship.x = canvas.width - ship.radius;
-   }else if (ship.x + ship.radius > canvas.width) {
-     ship.x = 0 + ship.radius;
-   }
 
-   if (ship.y - ship.radius < 0) {
-     ship.y = canvas.height - ship.radius;
-   }else if (ship.y + ship.radius > canvas.height) {
-     ship.y = 0 + ship.radius;
-   }
 
 
   clearCanvas();
   drawShip();
-  createAsteroids();
+  drawAsteroids();
+  boundaryCheck();
 }
+
+function boundaryCheck(){
+  //check canvas boundaries for collision and warp ship or asteroids to opposite side
+  if (ship.x - ship.radius < 0){
+    ship.x = canvas.width - ship.radius;
+  }else if (ship.x + ship.radius > canvas.width) {
+    ship.x = 0 + ship.radius;
+  }
+
+  if (ship.y - ship.radius < 0) {
+    ship.y = canvas.height - ship.radius;
+  }else if (ship.y + ship.radius > canvas.height) {
+    ship.y = 0 + ship.radius;
+  }
+
+  for (var i = 0; i < asteroids.length; i++) {
+    if (asteroids[i].x - asteroids[i].radius < 0){
+      asteroids[i].x = canvas.width - asteroids[i].radius;
+    }else if (asteroids[i].x + asteroids[i].radius > canvas.width) {
+      asteroids[i].x = 0 + asteroids[i].radius;
+    }
+
+    if (asteroids[i].y - asteroids[i].radius < 0) {
+      asteroids[i].y = canvas.height - asteroids[i].radius;
+    }else if (asteroids[i].y + asteroids[i].radius > canvas.height) {
+      asteroids[i].y = 0 + asteroids[i].radius;
+    }
+  }
+}
+
+createAsteroids();
+
 
 //Event handlers for game controls
 document.addEventListener('keydown', () => {
