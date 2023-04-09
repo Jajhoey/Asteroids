0644527f30696d3da694cd8665abf787a9176b11
Jajhoey <john.hoey15@my.stjohns.edu>
2023-04-09 02:05:13 -0400

    Added sound files

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 const startButton = document.getElementById('start');
+const retryButton = document.getElementById('retry');
+retryButton.classList.add('hidden')
 
 const turn_speed = 1080; //Degrees of rotation per second
 const friction = 0.7; //Coefficient of friction
@@ -8,15 +10,17 @@ const ship_acceleration = 20; //increase velocity by 20 pixels per second
 const rateOfFire = 3; // 3 shots per sec
 const bulletVel = 20; //pixels per sec
 const FPS = 60;
-const explodeDur = 3; //explode for 3 sec
+const explodeDur = 1; //explode for 1 sec
 const blinkDur = 3; //blink for 3 sec (invincibility)
+const levelDisplayDur = 2; //2 sec
+const restart = false;
+
 var numAsteroids = 3;
 var level = 0;
-var levelComplete = false;
 var currentLives = 3;
 var score = 0;
-
-
+var highScore = localStorage.highScore
+var tAlpha = 0; //decrease this to fade out level display
 let lasers = [];
 let asteroids = [];
 
@@ -24,6 +28,13 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var ship = newShip();
+var shipLife = {
+  x: ship.radius * 2,
+  y: ship.radius * 2,
+  angle: 90 / 180 * Math.PI,
+  radius: ship.radius
+}
+
 
 function displayInstructions(){
   context.font = '50px Times New Roman';
@@ -32,8 +43,7 @@ function displayInstructions(){
   context.fillText('Instructions', canvas.width/2, canvas.height/2 - 100);
   context.font = '20px Calibri';
   context.fillText('Use the arrow keys to rotate left and right. Use the up key to activate thrusters.', canvas.width/2, canvas.height/2);
-  context.fillText("Hold SPACE to shoot lasers!.", canvas.width/2, canvas.height/2 + 30);
-
+  context.fillText("Hold SPACE to shoot lasers!", canvas.width/2, canvas.height/2 + 30);
 }
 
 function newShip(){
@@ -112,8 +122,28 @@ function drawShip(e, b) {
       context.stroke();
     }
   }else {
-    shipExplode();
+    drawExplode(ship.x, ship.y, ship.radius);
   }
+}
+
+function drawExplode(x, y, r){
+  //draw explosion
+  context.strokeStyle = 'red';
+  context.lineWidth = r / 2;
+  context.fillStyle = 'orange';
+  context.beginPath();
+  context.arc(x, y + 10, r * 1.5, 0, Math.PI * 2);
+  context.stroke();
+  context.fill();
+  context.closePath();
+  context.strokeStyle = 'yellow'
+  context.lineWidth = r / 2;
+  context.fillStyle = 'white';
+  context.beginPath();
+  context.arc(x, y + 10, r, 0, Math.PI * 2);
+  context.stroke();
+  context.fill();
+  context.closePath();
 }
 
 function drawLasers(e){
@@ -185,8 +215,8 @@ function newAsteroid(astDebris){
     radius: ship.radius*2,
     angle: 90/180 * Math.PI,
     velocity: { //below syntax is multiplying by 1 if < 0.5, else multiply by -1
-      x: (Math.random() < .5 ? 1 : -1) * 2,
-      y: (Math.random() < .5 ? 1 : -1) * 2
+      x: (Math.random() < .5 ? 1 : -1) * 1.5,
+      y: (Math.random() < .5 ? 1 : -1) * 1.5
     },
     rotation: (Math.random() * 10 - 5) / 180 * Math.PI,
     vertices: 5 + Math.random() * 10,
@@ -196,12 +226,11 @@ function newAsteroid(astDebris){
     asteroid.vertJaggedness.push(Math.random()+1);
   }
   if (!astDebris) {
-    do {//reset the coords until the distance is larger than 5x asteroid radius for inital spawn
+    do {//reset the coords until the distance from ship is larger than 5x asteroid radius for inital spawn
       asteroid.x = Math.random() * canvas.width;
       asteroid.y = Math.random() * canvas.height;
     } while (distanceBetweenPoints(ship.x, ship.y, asteroid.x, asteroid.y) < asteroid.radius * 5);
   }
-  console.log("Radius is " + asteroid.radius);
   return asteroid;
 }
 
@@ -225,22 +254,22 @@ function astExplode(ast){
        asteroids.push(a);
      }
    }
-   asteroids.splice(asteroids.indexOf(ast), 1);
+  asteroids.splice(asteroids.indexOf(ast), 1);
 }
 
-var shipLife = {
-  x: ship.radius * 2,
-  y: ship.radius * 2,
-  angle: 90 / 180 * Math.PI,
-  radius: ship.radius
-}
-
-function drawLives(){
+function drawLives(last){
   for(var i = 0; i < currentLives; i++){
     //drawing each of the 3 lives, they are spaced out 
     shipLife.x = shipLife.radius * (2 + (i * 2.5));
-    
-    context.strokeStyle = 'white';
+    switch (last) {
+      case true:
+        context.strokeStyle = 'red';
+        break;
+      default:
+        context.strokeStyle = 'white';
+        break;
+    }
+
     context.lineWidth = ship.radius / 15;
     context.beginPath();
     context.moveTo(//ship's nose
@@ -261,11 +290,52 @@ function drawLives(){
 }
 
 function drawHighScore(){
-
+  if (highScore != null){
+    context.font = '50px Times New Roman';
+    context.fillStyle = 'white';
+    context.textAlign = 'center';
+    context.fillText('High Score: ' + highScore, canvas.width/2, ship.radius * 2);
+  }
 }
 
 function drawScore(){
+  context.font = '50px Times New Roman';
+  context.fillStyle = 'white';
+  context.textAlign = 'center';
+  context.fillText('Score: ' + score, canvas.width - ship.radius *  5, ship.radius * 2);
+}
 
+function displayLevel(){
+  if (tAlpha > 0 && currentLives > 0){
+    context.font = '50px Times New Roman';
+    context.fillStyle = 'rgba(255, 255, 255, ' + tAlpha + ')';
+    context.textAlign = 'center';
+    context.fillText('Level ' + level, canvas.width / 2, canvas.height / 4);
+  }
+}
+
+function nextLevel(){
+  if(currentLives > 0){
+    createAsteroids();
+    level += 1;
+  }
+}
+
+function gameOver(){
+  context.font = '50px Calibri';
+  context.fillStyle = 'red';
+  context.textAlign = 'center';
+  context.fillText("Game Over", canvas.width / 2, canvas.height / 2);
+  context.font = '30px Times New Roman';
+  context.fillStyle = 'white';
+  context.textAlign = 'center';
+  context.fillText("You made it to " + 'level ' + level, canvas.width / 2, canvas.height * 3/4);
+  
+  while(asteroids.length > 1){
+    asteroids.splice(0, asteroids.length);
+  }
+
+  retryButton.classList.remove('hidden');
 }
 
 function distanceBetweenPoints(x1, y1, x2, y2){// d=√((x2 – x1)² + (y2 – y1)²)
@@ -294,31 +364,6 @@ function showBounds(){
     context.closePath();
     context.stroke();
   }
-}
-
-function shipExplode(){
-  //draw explosion
-  context.strokeStyle = 'red';
-  context.lineWidth = ship.radius / 2;
-  context.fillStyle = 'orange';
-  context.beginPath();
-  context.arc(ship.x, ship.y + 10, ship.radius * 1.5, 0, Math.PI * 2);
-  context.stroke();
-  context.fill();
-  context.closePath();
-  context.strokeStyle = 'yellow'
-  context.lineWidth = ship.radius / 2;
-  context.fillStyle = 'white';
-  context.beginPath();
-  context.arc(ship.x, ship.y + 10, ship.radius, 0, Math.PI * 2);
-  context.stroke();
-  context.fill();
-  context.closePath();
-}
-
-function nextLevel(){
-  level += 1;
-  createAsteroids();
 }
 
 function boundaryWarp(){
@@ -361,11 +406,18 @@ function boundaryWarp(){
   }
 }
 
+var astExp = {
+  x: 0,
+  y: 0,
+  r: 0,
+  dur: 0
+}
 //this update function is the main game loop
 function update() {
   var isExploding = ship.explodeTime > 0;
-  var blink = ship.blinkTime % 5 == 0; //this line is tied to the frequency of blinking
-
+  var blink = ship.blinkTime % 5 == 0; //this line is tied to the frequency of blinking. Ship will blink on multiples of 5
+  var last = currentLives == 1;
+  
   //rotate the ship and asteroids
   ship.angle += ship.rotation / 180 * Math.PI;
   ship.rotation = 0;
@@ -430,29 +482,74 @@ function update() {
   for (var i = 0; i < lasers.length; i++) {
     for (var j = 0; j < asteroids.length; j++) {
       if (distanceBetweenPoints(lasers[i].x, lasers[i].y, asteroids[j].x, asteroids[j].y) < asteroids[j].radius * 1.25) {
-        astExplode(asteroids[j]);
+        astExploding = true;
+        astExp.x = asteroids[j].x;
+        astExp.y = asteroids[j].y;
+        astExp.r = asteroids[j].radius;
+        astExp.dur = explodeDur * FPS;
+        //Switch statement for determining how many points to add to score
+        switch (asteroids[j].radius) {
+          case ship.radius * 2:
+            score += 100;
+            break;
+          case ship.radius:
+            score += 75;
+            break;
+          case ship.radius/2:
+            score += 50;
+            break;
+          default:
+            break;
+        }  
+        astExplode(asteroids[j]);      
         lasers.splice(i, 1);
       }
     }
   }
 
+  //draw asteroid explosion
+  if (astExp.dur > 0) {
+    
+    drawExplode(astExp.x, astExp.y, astExp.r);
+    console.log(astExp.x, astExp.y, astExp.r);
+    astExp.dur--;
+  }
+
+  //updating high score
+  if (score >= highScore || highScore == null){
+    highScore = score;
+    localStorage.highScore = score;
+  }
+
+  //Next level when all asteroids are destroyed
+  
+
   if (asteroids.length == 0){
     nextLevel();
+    tAlpha = 1;
   }
 
 
   clearCanvas();
-  boundaryWarp();
 
-  //showBounds();
-  drawShip(isExploding, blink);
+  boundaryWarp();
+  
+  
+  if(currentLives > 0){
+    drawShip(isExploding, blink);
+  }else{
+    gameOver();
+  }
   drawAsteroids();
   drawLasers();
-  drawLives();
+  drawLives(last);
   drawHighScore();
   drawScore();
-
+  displayLevel();
+  tAlpha -= .01;
+  //showBounds();
 }
+displayLevel();
 
 //Event handlers for game controls DEPRECIATED
 document.addEventListener('keydown', () => {
@@ -495,18 +592,29 @@ document.addEventListener('keyup', () => {
 
 //debugging
 function test(){
+  //console.log('test');
 }
 
 function startGame(){
-  createAsteroids();
   setInterval(test, 1000);
   setInterval(update, 1000 / FPS); //Fps is 1/60th
   setInterval(createLasers, 1000 / rateOfFire);
 }
 
-//displayInstructions();
-//startButton.addEventListener('click', startGame);
-/*startButton.addEventListener('click', () => {
+function newGame(){
+  numAsteroids = 3;
+  level = 0;
+  currentLives = 3;
+  score = 0;
+}
+
+displayInstructions();
+startButton.addEventListener('click', startGame);
+startButton.addEventListener('click', () => {
   startButton.classList.add('hidden');
-});*/
-startGame();
+});
+retryButton.addEventListener('click', newGame);
+retryButton.addEventListener('click', () => {
+  retryButton.classList.add('hidden');
+});
+startGame()
